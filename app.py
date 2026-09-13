74c71bc86149042a587f5d66a21422338be4c1be
Ishika-om19 <somi37987@gmail.com>
2026-09-13 21:52:19 +0530

    Create careerr guidance form and Flask backend

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Career Guidance!"
+    return render_template("index.html")
+
+@app.route("/career-guidance", methods=["GET", "POST"])
+def career_guidance():
+    if request.method == "POST":
+        name = request.form["name"]
+        education = request.form["education"]
+        skills = request.form["skills"]
+        interests =  request.form["interests"]
+        subjects = request.form["subjects"]
+        strengths = request.form["strengths"]
+        career_preference = request.form["career_preference"]
+        goal = request.form["goal"]
+
+        print(name)
+        print(education)
+        print(skills)
+        print(interests)
+        print(subjects)
+        print(strengths)
+        print(career_preference)
+        print(goal)
+
+    return render_template("index.html")
 
 app.run(debug=True)
